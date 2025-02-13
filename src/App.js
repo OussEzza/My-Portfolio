e013fe39dc1d22dfcae37338bcc93e9c4bb041f4
EZZAHRI Oussama <ezzahrioussama01@gmail.com>
2025-02-13 13:39:22 +0100

    Update the App.js file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import AboutPage from "./components/AboutPage";
 import ProjectsPage from "./components/ProjectsPage";
 import EducationPage from "./components/EducationPage";
 import ContactPage from "./components/ContactPage";
 import HomePage from "./components/HomePage";
-// import FooterPage from "./components/FooterPage";
 import ActionBar from "./components/ActionBar";
+import PageNotFound from "./components/PageNotFound";
+import ThankYouPage from "./components/ThankYouPage";
 
 import "./index.css";
 
 function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/thank-you" element={<ThankYouPage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function MainPage() {
   return (
     <>
       <NavBar />
@@ -33,7 +47,6 @@ function App() {
       <footer className="py-8 bg-gray-800 text-white text-center">
         <p>&copy; 2025 Oussama EZZAHRI. All Rights Reserved.</p>
       </footer>
-      {/* <FooterPage /> */}
     </>
   );
 }
